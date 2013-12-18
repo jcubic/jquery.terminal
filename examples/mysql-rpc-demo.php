68c8b436e42629186d742458c464318e75864c72
Jakub Jankiewicz <jcubic@onet.pl>
2013-12-18 22:22:39 +0100

    silent mysql connection function #133

--- examples/mysql-rpc-demo.php
+++ examples/mysql-rpc-demo.php
@@ -4,8 +4,8 @@ require('json-rpc.php');
 
 if(function_exists('xdebug_disable')) { xdebug_disable(); }
 
-mysql_connect('localhost', 'user', 'password');
-mysql_select_db('database_name');
+@mysql_connect('localhost', 'user', 'password');
+@mysql_select_db('database_name');
 
 class MysqlDemo {
   public function query($query) {
