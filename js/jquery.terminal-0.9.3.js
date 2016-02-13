2c0e311b11ad6e71c2316c4e060717e734ba29db
Jakub Jankiewicz <jcubic@onet.pl>
2016-02-13 11:48:04 +0100

    Use short license info in source files

--- js/jquery.terminal-0.9.3.js
+++ js/jquery.terminal-0.9.3.js
@@ -4,30 +4,12 @@
  *  __ / // // // // // _  // _// // / / // _  // _//     // //  \/ // _ \/ /
  * /  / // // // // // ___// / / // / / // ___// / / / / // // /\  // // / /__
  * \___//____ \\___//____//_/ _\_  / /_//____//_/ /_/ /_//_//_/ /_/ \__\_\___/
- *           \/              /____/                              version 0.9.3
- * http://terminal.jcubic.pl
+ *           \/              /____/                              version {{VER}
+ *
+ * This file is part of jQuery Terminal. http://terminal.jcubic.pl
  *
  * Copyright (c) 2010-2016 Jakub Jankiewicz <http://jcubic.pl>
- *
- * This file is part of jQuery Terminal.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * Released under the MIT license
  *
  * Contains:
  *
@@ -49,7 +31,7 @@
  * Copyright (c) 2007-2013 Alexandru Marasteanu <hello at alexei dot ro>
  * licensed under 3 clause BSD license
  *
- * Date: Fri, 12 Feb 2016 08:14:29 +0000
+ * Date: Sat, 13 Feb 2016 10:47:32 +0000
  */
 
 /* TODO:
