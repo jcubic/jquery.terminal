b21506daaddc256f2df9a308aee0210eba7916ed
Jakub Jankiewicz <jcubic@onet.pl>
2017-01-06 17:30:53 +0100

    Add --size css variable

--- js/jquery.terminal.js
+++ js/jquery.terminal.js
@@ -31,7 +31,7 @@
  * Copyright (c) 2007-2013 Alexandru Marasteanu <hello at alexei dot ro>
  * licensed under 3 clause BSD license
  *
- * Date: Fri, 06 Jan 2017 13:01:41 +0000
+ * Date: Fri, 06 Jan 2017 16:30:27 +0000
  */
 
 /* TODO:
