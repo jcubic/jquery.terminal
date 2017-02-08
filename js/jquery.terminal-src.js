052750535fb547e005956eefa51030691ea03534
Jakub Jankiewicz <jcubic@onet.pl>
2017-02-08 14:54:25 +0100

    Add transparent and currentcolor to list of valid colors

--- js/jquery.terminal-src.js
+++ js/jquery.terminal-src.js
@@ -1161,9 +1161,11 @@
                 self.set(history.previous());
             }
             first_up_history = false;
+            return false;
         }
         function next_history() {
             self.set(history.end() ? last_command : history.next());
+            return false;
         }
         function left() {
             if (position > 0) {
@@ -2036,39 +2038,39 @@
         version: '{{VER}}',
         // colors from http://www.w3.org/wiki/CSS/Properties/color/keywords
         color_names: [
-            'black', 'silver', 'gray', 'white', 'maroon', 'red', 'purple',
-            'fuchsia', 'green', 'lime', 'olive', 'yellow', 'navy', 'blue',
-            'teal', 'aqua', 'aliceblue', 'antiquewhite', 'aqua', 'aquamarine',
-            'azure', 'beige', 'bisque', 'black', 'blanchedalmond', 'blue',
-            'blueviolet', 'brown', 'burlywood', 'cadetblue', 'chartreuse',
-            'chocolate', 'coral', 'cornflowerblue', 'cornsilk', 'crimson',
-            'cyan', 'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgray',
-            'darkgreen', 'darkgrey', 'darkkhaki', 'darkmagenta',
-            'darkolivegreen', 'darkorange', 'darkorchid', 'darkred',
-            'darksalmon', 'darkseagreen', 'darkslateblue', 'darkslategray',
-            'darkslategrey', 'darkturquoise', 'darkviolet', 'deeppink',
-            'deepskyblue', 'dimgray', 'dimgrey', 'dodgerblue', 'firebrick',
-            'floralwhite', 'forestgreen', 'fuchsia', 'gainsboro', 'ghostwhite',
-            'gold', 'goldenrod', 'gray', 'green', 'greenyellow', 'grey',
-            'honeydew', 'hotpink', 'indianred', 'indigo', 'ivory', 'khaki',
-            'lavender', 'lavenderblush', 'lawngreen', 'lemonchiffon',
-            'lightblue', 'lightcoral', 'lightcyan', 'lightgoldenrodyellow',
-            'lightgray', 'lightgreen', 'lightgrey', 'lightpink', 'lightsalmon',
-            'lightseagreen', 'lightskyblue', 'lightslategray', 'lightslategrey',
-            'lightsteelblue', 'lightyellow', 'lime', 'limegreen', 'linen',
-            'magenta', 'maroon', 'mediumaquamarine', 'mediumblue',
-            'mediumorchid', 'mediumpurple', 'mediumseagreen', 'mediumslateblue',
-            'mediumspringgreen', 'mediumturquoise', 'mediumvioletred',
-            'midnightblue', 'mintcream', 'mistyrose', 'moccasin', 'navajowhite',
-            'navy', 'oldlace', 'olive', 'olivedrab', 'orange', 'orangered',
-            'orchid', 'palegoldenrod', 'palegreen', 'paleturquoise',
-            'palevioletred', 'papayawhip', 'peachpuff', 'peru', 'pink', 'plum',
-            'powderblue', 'purple', 'red', 'rosybrown', 'royalblue',
-            'saddlebrown', 'salmon', 'sandybrown', 'seagreen', 'seashell',
-            'sienna', 'silver', 'skyblue', 'slateblue', 'slategray',
-            'slategrey', 'snow', 'springgreen', 'steelblue', 'tan', 'teal',
-            'thistle', 'tomato', 'turquoise', 'violet', 'wheat', 'white',
-            'whitesmoke', 'yellow', 'yellowgreen'],
+            'transparent', 'currentcolor', 'black', 'silver', 'gray', 'white',
+            'maroon', 'red', 'purple', 'fuchsia', 'green', 'lime', 'olive',
+            'yellow', 'navy', 'blue', 'teal', 'aqua', 'aliceblue',
+            'antiquewhite', 'aqua', 'aquamarine', 'azure', 'beige', 'bisque',
+            'black', 'blanchedalmond', 'blue', 'blueviolet', 'brown',
+            'burlywood', 'cadetblue', 'chartreuse', 'chocolate', 'coral',
+            'cornflowerblue', 'cornsilk', 'crimson', 'cyan', 'darkblue',
+            'darkcyan', 'darkgoldenrod', 'darkgray', 'darkgreen', 'darkgrey',
+            'darkkhaki', 'darkmagenta', 'darkolivegreen', 'darkorange',
+            'darkorchid', 'darkred', 'darksalmon', 'darkseagreen',
+            'darkslateblue', 'darkslategray', 'darkslategrey', 'darkturquoise',
+            'darkviolet', 'deeppink', 'deepskyblue', 'dimgray', 'dimgrey',
+            'dodgerblue', 'firebrick', 'floralwhite', 'forestgreen', 'fuchsia',
+            'gainsboro', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green',
+            'greenyellow', 'grey', 'honeydew', 'hotpink', 'indianred', 'indigo',
+            'ivory', 'khaki', 'lavender', 'lavenderblush', 'lawngreen',
+            'lemonchiffon', 'lightblue', 'lightcoral', 'lightcyan',
+            'lightgoldenrodyellow', 'lightgray', 'lightgreen', 'lightgrey',
+            'lightpink', 'lightsalmon', 'lightseagreen', 'lightskyblue',
+            'lightslategray', 'lightslategrey', 'lightsteelblue', 'lightyellow',
+            'lime', 'limegreen', 'linen', 'magenta', 'maroon',
+            'mediumaquamarine', 'mediumblue', 'mediumorchid', 'mediumpurple',
+            'mediumseagreen', 'mediumslateblue', 'mediumspringgreen',
+            'mediumturquoise', 'mediumvioletred', 'midnightblue', 'mintcream',
+            'mistyrose', 'moccasin', 'navajowhite', 'navy', 'oldlace', 'olive',
+            'olivedrab', 'orange', 'orangered', 'orchid', 'palegoldenrod',
+            'palegreen', 'paleturquoise', 'palevioletred', 'papayawhip',
+            'peachpuff', 'peru', 'pink', 'plum', 'powderblue', 'purple', 'red',
+            'rosybrown', 'royalblue', 'saddlebrown', 'salmon', 'sandybrown',
+            'seagreen', 'seashell', 'sienna', 'silver', 'skyblue', 'slateblue',
+            'slategray', 'slategrey', 'snow', 'springgreen', 'steelblue', 'tan',
+            'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'wheat',
+            'white', 'whitesmoke', 'yellow', 'yellowgreen'],
         // ---------------------------------------------------------------------
         // :: Validate html color (it can be name or hex)
         // ---------------------------------------------------------------------
