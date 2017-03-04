791c025f8633e11e4cd32b2a332fc8e780bea557
Jakub Jankiewicz <jcubic@onet.pl>
2017-03-04 11:44:21 +0100

    Version 1.0.9

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=1.0.8
+VERSION=1.0.9
 UGLIFY=uglifyjs
 SED=sed
 CP=cp
--- js/jquery.terminal.js
+++ js/jquery.terminal.js
@@ -4,7 +4,7 @@
  *  __ / // // // // // _  // _// // / / // _  // _//     // //  \/ // _ \/ /
  * /  / // // // // // ___// / / // / / // ___// / / / / // // /\  // // / /__
  * \___//____ \\___//____//_/ _\_  / /_//____//_/ /_/ /_//_//_/ /_/ \__\_\___/
- *           \/              /____/                              version 1.0.8
+ *           \/              /____/                              version 1.0.9
  *
  * This file is part of jQuery Terminal. http://terminal.jcubic.pl
  *
@@ -31,7 +31,7 @@
  * Copyright (c) 2007-2013 Alexandru Marasteanu <hello at alexei dot ro>
  * licensed under 3 clause BSD license
  *
- * Date: Sat, 04 Mar 2017 10:14:38 +0000
+ * Date: Sat, 04 Mar 2017 10:44:08 +0000
  */
 
 /* TODO:
@@ -2075,7 +2075,7 @@
     var re_re = /^\/((?:\\\/|[^/]|\[[^\]]*\/[^\]]*\])+)\/([gimy]*)$/;
     /* eslint-enable */
     $.terminal = {
-        version: '1.0.8',
+        version: '1.0.9',
         // colors from http://www.w3.org/wiki/CSS/Properties/color/keywords
         color_names: [
             'transparent', 'currentcolor', 'black', 'silver', 'gray', 'white',
