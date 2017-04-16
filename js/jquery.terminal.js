5de9b0c1fbc22bc7afae715799c8018a9dd6302b
Jakub Jankiewicz <jcubic@onet.pl>
2017-04-16 18:06:24 +0200

    fix minified css (cssnano was removing unused animations)

--- js/jquery.terminal.js
+++ js/jquery.terminal.js
@@ -31,7 +31,7 @@
  * Copyright (c) 2007-2013 Alexandru Marasteanu <hello at alexei dot ro>
  * licensed under 3 clause BSD license
  *
- * Date: Sun, 16 Apr 2017 15:20:24 +0000
+ * Date: Sun, 16 Apr 2017 16:06:01 +0000
  */
 
 /* TODO:
