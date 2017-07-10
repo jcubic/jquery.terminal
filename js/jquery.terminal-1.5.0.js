fda7424b031b225557d03393dbf40bebdb7b723f
Jakub Jankiewicz <jcubic@onet.pl>
2017-07-10 11:10:23 +0200

    version 1.5.0

--- js/jquery.terminal-1.5.0.js
+++ js/jquery.terminal-1.5.0.js
@@ -4,7 +4,7 @@
  *  __ / // // // // // _  // _// // / / // _  // _//     // //  \/ // _ \/ /
  * /  / // // // // // ___// / / // / / // ___// / / / / // // /\  // // / /__
  * \___//____ \\___//____//_/ _\_  / /_//____//_/ /_/ /_//_//_/ /_/ \__\_\___/
- *           \/              /____/                              version 1.5
+ *           \/              /____/                              version 1.5.0
  *
  * This file is part of jQuery Terminal. http://terminal.jcubic.pl
  *
@@ -31,7 +31,7 @@
  * Copyright (c) 2007-2013 Alexandru Marasteanu <hello at alexei dot ro>
  * licensed under 3 clause BSD license
  *
- * Date: Mon, 10 Jul 2017 08:42:13 +0000
+ * Date: Mon, 10 Jul 2017 09:10:00 +0000
  */
 
 /* TODO:
@@ -2347,7 +2347,7 @@
     var unclosed_strings_re = /^(?=((?:[^"']+|"[^"\\]*(?:\\[^][^"\\]*)*"|'[^'\\]*(?:\\[^][^'\\]*)*')*))\1./;
     /* eslint-enable */
     $.terminal = {
-        version: '1.5',
+        version: '1.5.0',
         // colors from http://www.w3.org/wiki/CSS/Properties/color/keywords
         color_names: [
             'transparent', 'currentcolor', 'black', 'silver', 'gray', 'white',
