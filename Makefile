2172094ca5021453742e251eb06f9239f3618e8a
Jakub Jankiewicz <jcubic@onet.pl>
2017-11-07 20:45:54 +0100

    version 1.10.0

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=1.9.0
+VERSION=1.10.0
 SED=sed
 CD=cd
 NPM=npm
--- js/jquery.terminal-1.10.0.js
+++ js/jquery.terminal-1.10.0.js
@@ -4,7 +4,7 @@
  *  __ / // // // // // _  // _// // / / // _  // _//     // //  \/ // _ \/ /
  * /  / // // // // // ___// / / // / / // ___// / / / / // // /\  // // / /__
  * \___//____ \\___//____//_/ _\_  / /_//____//_/ /_/ /_//_//_/ /_/ \__\_\___/
- *           \/              /____/                              version DEV
+ *           \/              /____/                              version 1.10.0
  *
  * This file is part of jQuery Terminal. http://terminal.jcubic.pl
  *
@@ -32,7 +32,7 @@
  * Copyright (c) 2007-2013 Alexandru Marasteanu <hello at alexei dot ro>
  * licensed under 3 clause BSD license
  *
- * Date: Tue, 07 Nov 2017 09:30:12 +0000
+ * Date: Tue, 07 Nov 2017 19:45:46 +0000
  */
 
 /* TODO:
@@ -2722,8 +2722,8 @@
         }
     }
     $.terminal = {
-        version: 'DEV',
-        date: 'Tue, 07 Nov 2017 09:30:12 +0000',
+        version: '1.10.0',
+        date: 'Tue, 07 Nov 2017 19:45:46 +0000',
         // colors from http://www.w3.org/wiki/CSS/Properties/color/keywords
         color_names: [
             'transparent', 'currentcolor', 'black', 'silver', 'gray', 'white',
