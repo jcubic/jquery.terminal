2d770eadb187509929206889f14b9429ddaa48cf
Jakub Jankiewicz <jcubic@onet.pl>
2018-06-04 21:48:08 +0200

    fix package.json

--- js/jquery.terminal.js
+++ js/jquery.terminal.js
@@ -4,7 +4,7 @@
  *  __ / // // // // // _  // _// // / / // _  // _//     // //  \/ // _ \/ /
  * /  / // // // // // ___// / / // / / // ___// / / / / // // /\  // // / /__
  * \___//____ \\___//____//_/ _\_  / /_//____//_/ /_/ /_//_//_/ /_/ \__\_\___/
- *           \/              /____/                              version DEV
+ *           \/              /____/                              version 1.16.0
  *
  * This file is part of jQuery Terminal. http://terminal.jcubic.pl
  *
@@ -32,7 +32,7 @@
  * Copyright (c) 2007-2013 Alexandru Marasteanu <hello at alexei dot ro>
  * licensed under 3 clause BSD license
  *
- * Date: Mon, 04 Jun 2018 19:39:57 +0000
+ * Date: Mon, 04 Jun 2018 19:47:45 +0000
  */
 
 /* TODO:
@@ -2899,8 +2899,8 @@
     }
     // -------------------------------------------------------------------------
     $.terminal = {
-        version: 'DEV',
-        date: 'Mon, 04 Jun 2018 19:39:57 +0000',
+        version: '1.16.0',
+        date: 'Mon, 04 Jun 2018 19:47:45 +0000',
         // colors from http://www.w3.org/wiki/CSS/Properties/color/keywords
         color_names: [
             'transparent', 'currentcolor', 'black', 'silver', 'gray', 'white',
