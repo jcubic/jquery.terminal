377e1b519e479523371978d4ead4770ff72e7b2f
Jakub Jankiewicz <jcubic@onet.pl>
2018-08-19 12:06:30 +0200

    make signature in README smaller for npm

--- js/jquery.terminal-1.20.1.js
+++ js/jquery.terminal-1.20.1.js
@@ -4,7 +4,7 @@
  *  __ / // // // // // _  // _// // / / // _  // _//     // //  \/ // _ \/ /
  * /  / // // // // // ___// / / // / / // ___// / / / / // // /\  // // / /__
  * \___//____ \\___//____//_/ _\_  / /_//____//_/ /_/ /_//_//_/ /_/ \__\_\___/
- *           \/              /____/                              version 1.20.1
+ *           \/              /____/                              version DEV
  *
  * This file is part of jQuery Terminal. http://terminal.jcubic.pl
  *
@@ -32,7 +32,7 @@
  * Copyright (c) 2007-2013 Alexandru Marasteanu <hello at alexei dot ro>
  * licensed under 3 clause BSD license
  *
- * Date: Sun, 19 Aug 2018 09:48:57 +0000
+ * Date: Sun, 19 Aug 2018 10:06:14 +0000
  */
 
 /* TODO:
@@ -2954,8 +2954,8 @@
     }
     // -------------------------------------------------------------------------
     $.terminal = {
-        version: '1.20.1',
-        date: 'Sun, 19 Aug 2018 09:48:57 +0000',
+        version: 'DEV',
+        date: 'Sun, 19 Aug 2018 10:06:14 +0000',
         // colors from http://www.w3.org/wiki/CSS/Properties/color/keywords
         color_names: [
             'transparent', 'currentcolor', 'black', 'silver', 'gray', 'white',
