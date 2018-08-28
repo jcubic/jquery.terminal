59a931a65fde76a157da83b4450e040155dbcef2
Jakub Jankiewicz <jcubic@onet.pl>
2018-08-28 08:15:41 +0200

    fix cursor for PrismJS punctuation class

--- js/jquery.terminal-1.21.0.js
+++ js/jquery.terminal-1.21.0.js
@@ -4,7 +4,7 @@
  *  __ / // // // // // _  // _// // / / // _  // _//     // //  \/ // _ \/ /
  * /  / // // // // // ___// / / // / / // ___// / / / / // // /\  // // / /__
  * \___//____ \\___//____//_/ _\_  / /_//____//_/ /_/ /_//_//_/ /_/ \__\_\___/
- *           \/              /____/                              version 1.21.0
+ *           \/              /____/                              version DEV
  *
  * This file is part of jQuery Terminal. http://terminal.jcubic.pl
  *
@@ -32,7 +32,7 @@
  * Copyright (c) 2007-2013 Alexandru Marasteanu <hello at alexei dot ro>
  * licensed under 3 clause BSD license
  *
- * Date: Sun, 26 Aug 2018 10:37:15 +0000
+ * Date: Tue, 28 Aug 2018 06:14:42 +0000
  */
 
 /* TODO:
@@ -3119,8 +3119,8 @@
     }
     // -------------------------------------------------------------------------
     $.terminal = {
-        version: '1.21.0',
-        date: 'Sun, 26 Aug 2018 10:37:15 +0000',
+        version: 'DEV',
+        date: 'Tue, 28 Aug 2018 06:14:42 +0000',
         // colors from http://www.w3.org/wiki/CSS/Properties/color/keywords
         color_names: [
             'transparent', 'currentcolor', 'black', 'silver', 'gray', 'white',
