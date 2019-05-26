758bae1d9d709c52d0cd8845889f389e9426ec95
Jakub Jankiewicz <jcubic@onet.pl>
2019-05-27 00:32:38 +0200

    version 2.6.1

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=2.6.0
+VERSION=2.6.1
 SED=sed
 CD=cd
 NPM=npm
--- js/jquery.terminal-2.6.1.js
+++ js/jquery.terminal-2.6.1.js
@@ -4,7 +4,7 @@
  *  __ / // // // // // _  // _// // / / // _  // _//     // //  \/ // _ \/ /
  * /  / // // // // // ___// / / // / / // ___// / / / / // // /\  // // / /__
  * \___//____ \\___//____//_/ _\_  / /_//____//_/ /_/ /_//_//_/ /_/ \__\_\___/
- *           \/              /____/                              version DEV
+ *           \/              /____/                              version 2.6.1
  *
  * This file is part of jQuery Terminal. https://terminal.jcubic.pl
  *
@@ -39,7 +39,7 @@
  * emoji regex v7.0.1 by Mathias Bynens
  * MIT license
  *
- * Date: Sun, 26 May 2019 22:16:49 +0000
+ * Date: Sun, 26 May 2019 22:32:07 +0000
  */
 /* global location, setTimeout, window, global, sprintf, setImmediate,
           IntersectionObserver,  ResizeObserver, module, require, define,
@@ -3893,8 +3893,8 @@
     }
     // -------------------------------------------------------------------------
     $.terminal = {
-        version: 'DEV',
-        date: 'Sun, 26 May 2019 22:16:49 +0000',
+        version: '2.6.1',
+        date: 'Sun, 26 May 2019 22:32:07 +0000',
         // colors from https://www.w3.org/wiki/CSS/Properties/color/keywords
         color_names: [
             'transparent', 'currentcolor', 'black', 'silver', 'gray', 'white',
