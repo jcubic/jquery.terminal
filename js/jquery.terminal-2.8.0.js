d0cb6b57f4e171f0a13dc5772342eafaeb73ffca
Jakub Jankiewicz <jcubic@onet.pl>
2019-08-29 19:22:14 +0200

    version 2.8.0

--- js/jquery.terminal-2.8.0.js
+++ js/jquery.terminal-2.8.0.js
@@ -4,7 +4,7 @@
  *  __ / // // // // // _  // _// // / / // _  // _//     // //  \/ // _ \/ /
  * /  / // // // // // ___// / / // / / // ___// / / / / // // /\  // // / /__
  * \___//____ \\___//____//_/ _\_  / /_//____//_/ /_/ /_//_//_/ /_/ \__\_\___/
- *           \/              /____/                              version DEV
+ *           \/              /____/                              version 2.8.0
  *
  * This file is part of jQuery Terminal. https://terminal.jcubic.pl
  *
@@ -39,7 +39,7 @@
  * emoji regex v7.0.1 by Mathias Bynens
  * MIT license
  *
- * Date: Thu, 29 Aug 2019 15:20:37 +0000
+ * Date: Thu, 29 Aug 2019 17:22:02 +0000
  */
 /* global location, setTimeout, window, global, sprintf, setImmediate,
           IntersectionObserver,  ResizeObserver, module, require, define,
@@ -3931,8 +3931,8 @@
     }
     // -------------------------------------------------------------------------
     $.terminal = {
-        version: 'DEV',
-        date: 'Thu, 29 Aug 2019 15:20:37 +0000',
+        version: '2.8.0',
+        date: 'Thu, 29 Aug 2019 17:22:02 +0000',
         // colors from https://www.w3.org/wiki/CSS/Properties/color/keywords
         color_names: [
             'transparent', 'currentcolor', 'black', 'silver', 'gray', 'white',
