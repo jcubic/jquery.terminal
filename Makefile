da064c8a1b6faffab2e896131fd2593999a8b802
Jakub Jankiewicz <jcubic@onet.pl>
2020-01-01 18:03:39 +0100

    version 2.12.0

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=2.11.1
+VERSION=2.12.0
 SED=sed
 CD=cd
 NPM=npm
--- js/jquery.terminal-2.12.0.js
+++ js/jquery.terminal-2.12.0.js
@@ -4,7 +4,7 @@
  *  __ / // // // // // _  // _// // / / // _  // _//     // //  \/ // _ \/ /
  * /  / // // // // // ___// / / // / / // ___// / / / / // // /\  // // / /__
  * \___//____ \\___//____//_/ _\_  / /_//____//_/ /_/ /_//_//_/ /_/ \__\_\___/
- *           \/              /____/                              version DEV
+ *           \/              /____/                              version 2.12.0
  *
  * This file is part of jQuery Terminal. https://terminal.jcubic.pl
  *
@@ -41,7 +41,7 @@
  *
  * broken image by Sophia Bai from the Noun Project (CC-BY)
  *
- * Date: Wed, 01 Jan 2020 13:17:25 +0000
+ * Date: Wed, 01 Jan 2020 17:03:16 +0000
  */
 /* global location, setTimeout, window, global, sprintf, setImmediate,
           IntersectionObserver,  ResizeObserver, module, require, define,
@@ -4137,8 +4137,8 @@
     }
     // -------------------------------------------------------------------------
     $.terminal = {
-        version: 'DEV',
-        date: 'Wed, 01 Jan 2020 13:17:25 +0000',
+        version: '2.12.0',
+        date: 'Wed, 01 Jan 2020 17:03:16 +0000',
         // colors from https://www.w3.org/wiki/CSS/Properties/color/keywords
         color_names: [
             'transparent', 'currentcolor', 'black', 'silver', 'gray', 'white',
