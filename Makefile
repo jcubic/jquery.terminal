06039d7d8a30253fa1bf6160d9d4126006dc2e75
Jakub Jankiewicz <jcubic@onet.pl>
2021-02-14 12:03:17 +0100

    version 2.22.0

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=2.21.0
+VERSION=2.22.0
 SED=sed
 CD=cd
 NPM=npm
--- js/jquery.terminal-2.22.0.js
+++ js/jquery.terminal-2.22.0.js
@@ -4,7 +4,7 @@
  *  __ / // // // // // _  // _// // / / // _  // _//     // //  \/ // _ \/ /
  * /  / // // // // // ___// / / // / / // ___// / / / / // // /\  // // / /__
  * \___//____ \\___//____//_/ _\_  / /_//____//_/ /_/ /_//_//_/ /_/ \__\_\___/
- *           \/              /____/                              version DEV
+ *           \/              /____/                              version 2.22.0
  *
  * This file is part of jQuery Terminal. https://terminal.jcubic.pl
  *
@@ -41,7 +41,7 @@
  *
  * broken image by Sophia Bai from the Noun Project (CC-BY)
  *
- * Date: Sat, 13 Feb 2021 08:21:01 +0000
+ * Date: Sun, 14 Feb 2021 11:03:09 +0000
  */
 /* global define, Map */
 /* eslint-disable */
@@ -4761,8 +4761,8 @@
     }
     // -------------------------------------------------------------------------
     $.terminal = {
-        version: 'DEV',
-        date: 'Sat, 13 Feb 2021 08:21:01 +0000',
+        version: '2.22.0',
+        date: 'Sun, 14 Feb 2021 11:03:09 +0000',
         // colors from https://www.w3.org/wiki/CSS/Properties/color/keywords
         color_names: [
             'transparent', 'currentcolor', 'black', 'silver', 'gray', 'white',
