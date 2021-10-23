8fd2a6ed79d8e4d54aeaf06675a2109ce18f98b7
Jakub Jankiewicz <jcubic@onet.pl>
2021-10-23 10:35:27 +0200

    add `scrollbar-gutter` to main content

--- js/jquery.terminal.js
+++ js/jquery.terminal.js
@@ -4,7 +4,7 @@
  *  __ / // // // // // _  // _// // / / // _  // _//     // //  \/ // _ \/ /
  * /  / // // // // // ___// / / // / / // ___// / / / / // // /\  // // / /__
  * \___//____ \\___//____//_/ _\_  / /_//____//_/ /_/ /_//_//_/ /_/ \__\_\___/
- *           \/              /____/                              version 2.29.5
+ *           \/              /____/                              version DEV
  *
  * This file is part of jQuery Terminal. https://terminal.jcubic.pl
  *
@@ -41,7 +41,7 @@
  *
  * broken image by Sophia Bai from the Noun Project (CC-BY)
  *
- * Date: Sat, 16 Oct 2021 07:40:49 +0000
+ * Date: Sat, 23 Oct 2021 08:35:03 +0000
  */
 /* global define, Map */
 /* eslint-disable */
@@ -5070,8 +5070,8 @@
     }
     // -------------------------------------------------------------------------
     $.terminal = {
-        version: '2.29.5',
-        date: 'Sat, 16 Oct 2021 07:40:49 +0000',
+        version: 'DEV',
+        date: 'Sat, 23 Oct 2021 08:35:03 +0000',
         // colors from https://www.w3.org/wiki/CSS/Properties/color/keywords
         color_names: [
             'transparent', 'currentcolor', 'black', 'silver', 'gray', 'white',
