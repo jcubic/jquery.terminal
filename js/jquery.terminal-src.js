bacab1e609a8174ec8757cdf21e8dcb7c8802add
cowuake <riccardo_mura@yahoo.com>
2024-04-28 21:09:21 +0200

    Revert "Refactor fix for mobile zoom"
    This reverts commit 528164cd5087754cb77bf9769650785f2b788068.

--- js/jquery.terminal-src.js
+++ js/jquery.terminal-src.js
@@ -5297,14 +5297,16 @@
     // :: is that it's triggers when virtual keyboard is toggled
     // -------------------------------------------------------------------------
     function on_height_change(callback) {
+        var height = window.visualViewport.height;
         var scale = window.visualViewport.scale;
-        var height = Math.round(window.visualViewport.height * scale);
+        height = Math.round(height * scale);
         callback(height);
         window.visualViewport.addEventListener('resize', function() {
+            var newHeight = window.visualViewport.height;
             var newScale = window.visualViewport.scale;
-            var newHeight = Math.round(window.visualViewport.height * newScale);
             if (height !== newHeight) {
-                callback(newHeight);
+                height = Math.round(newHeight * newScale);
+                callback(height);
             }
         });
     }
