0c509fde441cd014e928b599dee2ef5da23d660b
Jakub Jankiewicz <jcubic@onet.pl>
2024-09-13 20:29:06 +0200

    tweak contributors list component

--- docs/src/components/Contributors/index.tsx
+++ docs/src/components/Contributors/index.tsx
@@ -18,8 +18,8 @@ export default function Contributors() {
         {users.map((user: User) => {
           return (
             <li key={user.login}>
-              <a href={user.url} rel="nofollow noopener">
-                <img src={user.avatar} alt={user.name} />
+              <a href={user.url} rel="nofollow noopener" title={user.name}>
+                <img src={user.avatar} alt={`Avatar of ${user.name}`} />
               </a>
             </li>
           );
