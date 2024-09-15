d12fd03ec36d91f22fc9e6efec39e4bbd761ff13
Jakub Jankiewicz <jcubic@onet.pl>
2024-09-15 22:27:58 +0200

    shake terminal on mousewheel when no scrollbar

--- docs/src/components/Terminal/terminal.ts
+++ docs/src/components/Terminal/terminal.ts
@@ -1,9 +1,32 @@
+import 'jquery.terminal';
+
+
+function scroll() {
+  const { length } = this.get_output().split('\n');
+  const rows = this.rows();
+  if (rows >= length) {
+    this.removeClass('shake');
+    this.addClass('shake');
+    this.stopTime('shake');
+    this.oneTime(200, 'shake', () => {
+      this.removeClass('shake');
+    });
+  }
+}
+
 export function initTerminal() {
   const $ = globalThis.$;
   const $term = $('.term');
   $term.empty();
 
-  $term.terminal();
+  const scroll_event: JQueryTerminal.MouseWheelCallback = (...args) => {
+    return scroll.apply(term, args);
+  };
+
+  const term = $term.terminal({}, {
+    mousewheel: scroll_event,
+    touchscroll: scroll_event
+  });
 
   return $term;
 };
