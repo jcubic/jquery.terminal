323a288b3568b0bbd1c2864e9052da127bf61ef6
Jakub Jankiewicz <jcubic@onet.pl>
2024-10-27 12:51:02 +0100

    (docs) add zod to typecheck joke API

--- docs/src/components/Terminal/joke.ts
+++ docs/src/components/Terminal/joke.ts
@@ -1,25 +1,44 @@
 import type { JQueryTerminal } from 'jquery.terminal';
 
+import { z } from 'zod';
+
+const singleJokeSchema = z.object({
+  type: z.literal('single'),
+  joke: z.string()
+});
+
+const twoPartJokeSchema = z.object({
+  type: z.literal('twopart'),
+  setup: z.string(),
+  delivery: z.string()
+});
+
+const jokeSchema = z.union([singleJokeSchema, twoPartJokeSchema]);
+
 export default async function joke(this: JQueryTerminal) {
   const res = await fetch('https://v2.jokeapi.dev/joke/Programming');
-  const data = await res.json();
+  const json = await res.json();
+
+  const { data, success } = jokeSchema.safeParse(json);
+
+  if (!success) {
+    return this.error("Error: Invalid joke data format");
+  }
+
+  const options = {
+    delay: 50,
+    typing: true,
+    keepWords: true
+  };
+
   (async () => {
     if (data.type == 'twopart') {
       this.animation(async () => {
-        await this.echo(`Q: ${data.setup}`, {
-          delay: 50,
-          typing: true
-        });
-        await this.echo(`A: ${data.delivery}`, {
-          delay: 50,
-          typing: true
-        });
+        await this.echo(`Q: ${data.setup}`, options);
+        await this.echo(`A: ${data.delivery}`, options);
       });
     } else if (data.type === 'single') {
-      await this.echo(data.joke, {
-        delay: 50,
-        typing: true
-      });
+      await this.echo(data.joke, options);
     }
   })();
 }
