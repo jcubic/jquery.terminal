948f0cbecf7c94bf8450a8f044f99f0215c8b11a
Jakub Jankiewicz <jcubic@onet.pl>
2024-11-18 16:27:42 +0100

    (docs) add firebase config

---
+++ docs/src/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+
+const firebase_config = {
+    apiKey: 'AIzaSyCJhLo__GsvoEcP3Tp8G5jAhMo0OLPuBec',
+    authDomain: 'jcubic-1500107003772.firebaseapp.com',
+    databaseURL: 'https://jcubic-1500107003772.firebaseio.com',
+    projectId: 'jcubic-1500107003772',
+    storageBucket: 'jcubic-1500107003772.appspot.com',
+    messagingSenderId: '1005897028349',
+    appId: '1:1005897028349:web:fc2d0f5524864d5d17e494'
+};
+
+export const vapid_key = 'BCwx3BcH1YFCjBfWy3qmEZyVukPf-cLZUfVe9j3j1bwRcYi1aVhiEHPZqBvAcnvSyfznGoRHQrv3I2fDyBfULxk';
+
+export const firebase = initializeApp(firebase_config);
+
