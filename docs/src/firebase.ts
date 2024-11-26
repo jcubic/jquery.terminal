b90f9ead77e990756924c1ba61fdc7c9aa7cf883
Jakub Jankiewicz <jcubic@onet.pl>
2024-11-26 15:40:50 +0100

    (docs) remove Firebase messaging code

--- docs/src/firebase.ts
+++ docs/src/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getMessaging, getToken } from 'firebase/messaging';
+import { getDatabase } from "firebase/database";
 
 import EventEmitter from '@site/src/EventEmitter';
 
@@ -13,20 +13,6 @@ const firebase_config = {
     appId: '1:1005897028349:web:fc2d0f5524864d5d17e494'
 };
 
-const vapid_key = 'BCwx3BcH1YFCjBfWy3qmEZyVukPf-cLZUfVe9j3j1bwRcYi1aVhiEHPZqBvAcnvSyfznGoRHQrv3I2fDyBfULxk';
-
 export const firebase = initializeApp(firebase_config);
 
-const messaging = getMessaging(firebase);
-
-export async function register() {
-  const emitter = new EventEmitter();
-  const sw = await navigator.serviceWorker.register('/firebase-messaging-sw.js');
-  const listen = async () => {
-    const token = await getToken(messaging, { vapidKey: vapid_key, serviceWorkerRegistration: sw });
-    emitter.emit('token', token);
-  };
-  listen();
-  return emitter;
-}
-
+const database = getDatabase(firebase);
