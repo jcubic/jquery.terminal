ad1708db349f70971904236367ac56336c0bd16b
Jakub Jankiewicz <jcubic@onet.pl>
2024-12-18 00:09:36 +0100

    (docs) update documentation label

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -72,7 +72,7 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Documentation',
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
