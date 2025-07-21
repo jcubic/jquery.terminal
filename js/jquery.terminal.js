3156d2e4d7a87149e8374ea6aa354ee8867d7e1e
Jakub Jankiewicz <jcubic@onet.pl>
2025-07-22 00:45:52 +0200

    version 2.45.2

--- js/jquery.terminal.js
+++ js/jquery.terminal.js
@@ -4,7 +4,7 @@
  *  __ / // // // // // _  // _// // / / // _  // _//     // //  \/ // _ \/ /
  * /  / // // // // // ___// / / // / / // ___// / / / / // // /\  // // / /__
  * \___//____ \\___//____//_/ _\_  / /_//____//_/ /_/ /_//_//_/ /_/ \__\_\___/
- *           \/              /____/                              version 2.45.1
+ *           \/              /____/                              version 2.45.2
  *
  * This file is part of jQuery Terminal. https://terminal.jcubic.pl
  *
@@ -41,7 +41,7 @@
  *
  * broken image by Sophia Bai from the Noun Project (CC-BY)
  *
- * Date: Mon, 21 Jul 2025 22:40:37 +0000
+ * Date: Mon, 21 Jul 2025 22:45:43 +0000
  */
 /* global define, Map, BigInt */
 /* eslint-disable */
@@ -5467,8 +5467,8 @@
     }
     // -------------------------------------------------------------------------
     $.terminal = {
-        version: '2.45.1',
-        date: 'Mon, 21 Jul 2025 22:40:37 +0000',
+        version: '2.45.2',
+        date: 'Mon, 21 Jul 2025 22:45:43 +0000',
         // colors from https://www.w3.org/wiki/CSS/Properties/color/keywords
         color_names: [
             'transparent', 'currentcolor', 'black', 'silver', 'gray', 'white',
